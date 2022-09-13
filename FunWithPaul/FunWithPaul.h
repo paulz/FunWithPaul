fc1d27f19d1aee064cce8cc37ee48cc85bf4b7a6
Wesley Matlock <wmatlock@gmail.com>
2022-09-13 16:20:22 -0600

    Initial Commit

---
+++ FunWithPaul/FunWithPaul.h
@@ -0,0 +1,18 @@
+//
+//  FunWithPaul.h
+//  FunWithPaul
+//
+//  Created by Wesley Matlock on 9/13/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FunWithPaul.
+FOUNDATION_EXPORT double FunWithPaulVersionNumber;
+
+//! Project version string for FunWithPaul.
+FOUNDATION_EXPORT const unsigned char FunWithPaulVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunWithPaul/PublicHeader.h>
+
+
